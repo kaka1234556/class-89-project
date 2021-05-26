46408c55b4377a26b0bfcbb0ca7ddadc4bb091a0
kaka1234556 <80517682+kaka1234556@users.noreply.github.com>
2021-05-26 19:49:29 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -17,7 +17,7 @@ document.getElementById("answer").innerHTML = "Answer Turn-" + player2 ;
 function send()
  {
  number_1=document.getElementById("number_1").value;
- number_2=document.getElementById("number_1").value;
+ number_2=document.getElementById("number_2").value;
  actual_answer=parseInt(number_1) * parseInt(number_2);
 
 question_number="<h4>" + number_1 +"X"+number_2+"</h4>";
@@ -26,9 +26,59 @@ button = "<br><br><button onclick='check()' class='btn btn-info'>Check</button>"
 
 row = question_number+input_box+ button;
 document.getElementById("output").innerHTML=row;
-document.getElementById("word").value="";
 
 document.getElementById("number_1").value ="";
 document.getElementById("number_2").value ="";
 
  }
+
+ 
+ question_turn ="player1";
+ answer_turn ="player2"  ;
+
+
+ function check()
+ {
+   get_answer=document.getElementById("input_check_box").value;
+    
+    
+if(get_answer==actual_answer)
+    {
+    if(answer_turn =="player2"){
+    score2=score2+1;
+    document.getElementById("score_board2").innerHTML=score2;
+    }
+    else{
+        score1=score1+1;
+        document.getElementById("score_board1").innerHTML=score1;
+    }
+
+if(question_turn=="player1")
+{
+    question_turn="player2";
+    document.getElementById("question").innerHTML = "Question Turn-" + player2;
+
+}
+
+else{
+    question_turn="player1";
+    document.getElementById("question").innerHTML = "Question Turn-" + player1;
+
+}
+
+if(answer_turn=="player2")
+{
+    answer_turn="player1";
+    document.getElementById("answer").innerHTML = "Answer Turn- " + player1;
+
+}
+
+else{
+    answer_turn="player2";
+    document.getElementById("answer").innerHTML = "Answer Turn- " + player2;
+
+}
+ }
+ document.getElementById("output").innerHTML=""
+ }
+
